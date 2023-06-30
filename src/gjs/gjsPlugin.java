f6ac0323b9086182ba1a73bc204462d983ae753e
novarc <2124363741@qq.com>
2023-06-30 11:56:43 +0800

    packet

--- src/gjs/gjsPlugin.java
+++ src/gjs/gjsPlugin.java
@@ -4,6 +4,10 @@ import arc.util.CommandHandler;
 import arc.util.Log;
 import mindustry.mod.Plugin;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 public class gjsPlugin extends Plugin {
     @Override
     public void init() {
@@ -13,10 +17,18 @@ public class gjsPlugin extends Plugin {
     @Override
     public void registerServerCommands(CommandHandler handler) {
         handler.register("gjs","run js with Graaljs",(args, parameter) ->{
-            var str=new StringBuilder("");
+            var str=new StringBuilder();
             for(var i:args){
                 str.append(i);
             }
+            ScriptEngine eng=new ScriptEngineManager().getEngineByName("js");
+            try {
+                var res=eng.eval(str.toString());
+                Log.info("[GLS]:@",res.toString());
+            } catch (ScriptException e) {
+                Log.err("[GJS]:@",e.getMessage());
+            }
+
         });
     }
 }
