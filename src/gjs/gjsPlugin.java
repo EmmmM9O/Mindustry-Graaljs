3a4af5fdb04f6bcbff1627912b97d8c3055e8489
novarc <2124363741@qq.com>
2023-06-30 16:56:45 +0800

    aaa

--- src/gjs/gjsPlugin.java
+++ src/gjs/gjsPlugin.java
@@ -1,18 +1,47 @@
 package gjs;
 
+import arc.files.Fi;
 import arc.util.CommandHandler;
 import arc.util.Log;
+import arc.util.Timer;
+import mindustry.Vars;
 import mindustry.mod.Plugin;
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.PolyglotException;
 
-import java.util.HashMap;
+import java.util.*;
 
 public class gjsPlugin extends Plugin {
+
     public HashMap<String,Context> envs=new HashMap<>();
+    public Fi serverFi=Fi.get("/");
+    public Fi LogFi=Fi.get("/");
+
+    public void startLogTime(){
+        long time=60*60*12;
+        Timer.schedule(()->{
+            Log.info("[GJS]--------[arrange all logs]");
+            for(var f:LogFi.list()){
+                if(f.name().endsWith(".nlog")){
+                    var tmpFi=LogFi.child(f.name().substring(0,f.name().length()-4)+'-'+new Date().getTime()+ ".log");
+                    f.moveTo(tmpFi);
+
+                    Log.info("[GJS]:@",tmpFi.name());
+
+                }
+            }
+        },10,time);
+    }
     @Override
     public void init() {
-        Log.info("[Mod][Start]");
+        Log.info("[GJS][Start]");
+        serverFi= Vars.mods.getMod(gjsPlugin.class).file.parent().parent();
+        LogFi=serverFi.child("js-logs");
+        if(!LogFi.exists()){
+            LogFi.mkdirs();
+        }
+        Log.info("[GJS][will log in @]",LogFi.path());
+        startLogTime();
     }
 
     @Override
@@ -24,8 +53,10 @@ public class gjsPlugin extends Plugin {
                 str.append(args[k]);
             }
             if(!envs.containsKey(env)) {
-                envs.put(env,Context.newBuilder("js").build());
-                Log.info("[GJS]:[New Environment Start]");
+                String logFile=LogFi.child("log-"+env+ ".nlog").absolutePath();
+                envs.put(env,Context.newBuilder("js").option("log.file",logFile).build());
+                Log.info("[GJS]:[New Environment<@>Start]",env);
+                Log.info("[GJS]:[Log in @ ]",logFile);
             }
             var context=envs.get(env);
             try {
@@ -33,6 +64,30 @@ public class gjsPlugin extends Plugin {
                 Log.info("[GJS]:@", res.toString());
             }catch(PolyglotException error){
                 Log.err("[GJS]:@",error.getMessage());
+            }
+        });
+        handler.register("gjp","[class/packet] [env] [data]","",(args)->{
+            if(!envs.containsKey(args[1])){
+                Log.err("[GJP][No Env]");
+                return;
+            }
+            var env=envs.get(args[1]);
+            var data=args[2];
+            switch (args[0]){
+                case "class":
+                    try {
+                        var tmp=Class.forName(data);
+                        env.getBindings("js").putMember(data,tmp);
+
+                    } catch (ClassNotFoundException e) {
+                        Log.err("[GJP][Class][@]",e.getMessage());
+                    }
+                    break;
+                case "packet":
+
+                    break;
+                default:
+                    Log.err("[GJP][No Action]");
             }
         });
     }
